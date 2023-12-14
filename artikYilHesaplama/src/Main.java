4169102759e5d594129f80f266ffac7dc0b75191
yigitortu <yigittortu287@gmail.com>
2023-12-14 18:46:04 +0300

    artikYilHesaplama bitirildii

--- artikYilHesaplama/src/Main.java
+++ artikYilHesaplama/src/Main.java
@@ -11,20 +11,20 @@ public class Main {
         System.out.print("Yıl giriniz : ");
         year = inp.nextInt();
 
-        if (year >= 1 && year <= 999999){
+        if (year >= 1 && year <= 999999){   // Yıl aralığını belirttik
+            // Alınan yıl 4'ün katı olmalı seçeneği
             if (year % 4 == 0 ) {
-                if (year % 100 == 0 && year % 400 == 0) {
+                if (year % 100 == 0 && year % 400 == 0) { // 4'ün katı olan yılların 100'e ve 400'e bölünme kuralı
                     System.out.println(year + " Bir artık yıldır.");
                 }
                 else {
                     System.out.println(year + " Artık yıl değildir.");
                 }
-
+            // 4'e bölünmeyen yıllar için vereceğimiz mesaj.
             }else {
                 System.out.println(year + " Artık yıl değildir.");
             }
-
-
+            // Geçersiz bir yıl girdiği takdirde kullanıcıya verilen mesaj Örnek = 0
         }else {
             System.out.println("Geçersiz bir yıl girdiniz. tekrar deneyiniz.");
         }
